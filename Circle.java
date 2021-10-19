eef586e08f7f88bd4d2837397ada756b7651c6a8
september27 <backuprohit99@gmail.com>
2021-10-19 17:16:14 +0530

    calculate circle area

---
+++ Circle.java
@@ -0,0 +1,18 @@
+
+public class Circle {
+
+public static void main(String args [])
+{
+
+    int radius = 5;
+    double pi = 3.14;
+System.out.println("Area of circle : " + (pi*radius*radius));
+
+
+}
+
+
+
+}
+
+
